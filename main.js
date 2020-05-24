19e7a243b8e61ca3e368ea3afa3cdc0fb41f9230
Perrin Thibaud <thibaud.perrin@outlook.com>
2020-05-24 23:27:10 +0200

    Version 1.0.2 update

--- main.js
+++ main.js
@@ -17,8 +17,37 @@ function createWindow () {
         mainWindow = null;
     });
 
-    mainWindow.once('ready-to-show', () => {
-        autoUpdater.checkForUpdatesAndNotify();
+
+    // Let autoUpdater check for updates, it will start downloading it automatically
+    autoUpdater.checkForUpdates();
+
+    // Catch the update-available event
+    autoUpdater.addListener('update-available', (info) => {
+        mainWindow.webContents.send('update-available');
+    });
+
+    // Catch the update-not-available event
+    autoUpdater.addListener('update-not-available', (info) => {
+        mainWindow.webContents.send('update-not-available');
+    });
+
+    // Catch the download-progress events
+    autoUpdater.addListener('download-progress', (info) => {
+        mainWindow.webContents.send('prog-made');
+    });
+
+    // Catch the update-downloaded event
+    autoUpdater.addListener('update-downloaded', (info) => {
+        mainWindow.webContents.send('update-downloaded');
+    });
+
+    // Catch the error events
+    autoUpdater.addListener('error', (error) => {
+        mainWindow.webContents.send('error', error.toString());
+    });
+
+    ipcMain.on('quitAndInstall', (event, arg) => {
+        autoUpdater.quitAndInstall();
     });
 }
 
@@ -42,17 +71,14 @@ ipcMain.on('app_version', (event) => {
     event.sender.send('app_version', { version: app.getVersion() });
 });
 
-autoUpdater.on('update-available', () => {
+/*autoUpdater.on('update-available', () => {
     mainWindow.webContents.send('update_available');
 });
 
 autoUpdater.on('update-downloaded', () => {
     mainWindow.webContents.send('update_downloaded');
-});
+});*/
 
-ipcMain.on('restart_app', () => {
-    autoUpdater.quitAndInstall();
-});
 
 ipcMain.on('open_dev', () => {
     mainWindow.openDevTools();
